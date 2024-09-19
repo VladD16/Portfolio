ca5ad05574f8e4bc225aca5f8d1f176beae992ce
VladD16 <162788325+VladD16@users.noreply.github.com>
2024-09-19 12:18:31 +0300

    Add files via upload

---
+++ Growfy/js/script.js
@@ -0,0 +1,12 @@
+'use strict'
+
+// === Burger ==================================================
+
+document.addEventListener('click', function (e) {
+  const targetElement = e.target
+  if (targetElement.closest('.icon-menu')) {
+    document.documentElement.classList.toggle('menu-open')
+  }
+})
+
+// =============================================================
