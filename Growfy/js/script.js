2dc954665e017484d0a8720b1722719e1a5fa942
VladD16 <162788325+VladD16@users.noreply.github.com>
2024-09-23 15:29:13 +0300

    Add files via upload

--- Growfy/js/script.js
+++ Growfy/js/script.js
@@ -9,4 +9,38 @@ document.addEventListener('click', function (e) {
   }
 })
 
+// === Header Fixed ==========================================================
+
+let header = document.querySelector('header')
+let headerH = header.clientHeight
+document.onscroll = function () {
+  let scroll = window.scrollY
+  if (scroll > headerH) {
+    header.classList.add('fixed')
+    document.body.style.paddingTop = headerH + 'px'
+  } else {
+    header.classList.remove('fixed')
+    document.body.removeAttribute('style')
+  }
+}
+
+// === Scroll smooth ==========================================================
+
+const anchors = document.querySelectorAll('a[href*="#"]')
+
+for (let anchor of anchors) {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault()
+
+    const goto = anchor.hasAttribute('href')
+      ? anchor.getAttribute('href')
+      : 'body'
+
+    document.querySelector(goto).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start',
+    })
+  })
+}
+
 // =============================================================
