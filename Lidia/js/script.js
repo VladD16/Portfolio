15ffc211b33c7e11c9593ee77fa1f2deb2017e56
VladD16 <162788325+VladD16@users.noreply.github.com>
2024-10-07 16:06:50 +0300

    Add files via upload

--- Lidia/js/script.js
+++ Lidia/js/script.js
@@ -1,5 +1,7 @@
 'use strict'
 
+//////// Burger ///////////////////////////////////////////////
+
 document.addEventListener('click', function (e) {
   const targetElement = e.target
   if (targetElement.closest('.icon-menu')) {
@@ -9,7 +11,7 @@ document.addEventListener('click', function (e) {
   }
 })
 
-///////////////////////////////////////////////////////
+///////// Header fixed //////////////////////////////////////////////
 
 let header = document.querySelector('header')
 let headerH = header.clientHeight
@@ -24,7 +26,7 @@ document.onscroll = function () {
   }
 }
 
-///////////////////////////////////////////////////////
+////////// Scroll To /////////////////////////////////////////////
 
 const anchors = document.querySelectorAll('a[href*="#"]')
 const headerHeight = header.clientHeight
@@ -32,7 +34,34 @@ const headerHeight = header.clientHeight
 for (let anchor of anchors) {
   anchor.addEventListener('click', function (e) {
     e.preventDefault()
-
+    // ---------------------------------
+    const removeActiveClasses = () => {
+      anchors.forEach((anchor) => anchor.classList.remove('active'))
+    }
+    const addActiveClass = (id) => {
+      const activeAnchor = document.querySelector(`a[href="${id}"]`)
+      if (activeAnchor) {
+        activeAnchor.classList.add('active')
+      }
+    }
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            removeActiveClasses()
+            addActiveClass(`#${entry.target.id}`)
+          }
+        })
+      },
+      {
+        rootMargin: `-${headerHeight}px 0px 0px 0px`,
+        threshold: 0.1,
+      }
+    )
+    document.querySelectorAll('section[id]').forEach((section) => {
+      observer.observe(section)
+    })
+    // ---------------------------------
     const goto = anchor.hasAttribute('href')
       ? anchor.getAttribute('href')
       : 'body'
