97d8e48c03c5c115be62ede558ab7f34f68d0f19
Vladyslav Dubeiko <162788325+VladD16@users.noreply.github.com>
2025-02-22 14:57:18 +0200

    Add files via upload

--- Krovato/js/app.js
+++ Krovato/js/app.js
@@ -3783,6 +3783,9 @@
                 }), 0);
             }));
         }
+        function getHash() {
+            if (location.hash) return location.hash.replace("#", "");
+        }
         let _slideUp = (target, duration = 500, showmore = 0) => {
             if (!target.classList.contains("_slide")) {
                 target.classList.add("_slide");
@@ -4002,6 +4005,10 @@
                     document.documentElement.classList.toggle("menu-open");
                 }
             }));
+        }
+        function menuClose() {
+            bodyUnlock();
+            document.documentElement.classList.remove("menu-open");
         }
         function showMore() {
             window.addEventListener("load", (function(e) {
@@ -4404,6 +4411,44 @@
             }
         }
         modules_flsModules.popup = new Popup({});
+        let gotoblock_gotoBlock = (targetBlock, noHeader = false, speed = 500, offsetTop = 0) => {
+            const targetBlockElement = document.querySelector(targetBlock);
+            if (targetBlockElement) {
+                let headerItem = "";
+                let headerItemHeight = 0;
+                if (noHeader) {
+                    headerItem = "header.header";
+                    const headerElement = document.querySelector(headerItem);
+                    if (!headerElement.classList.contains("_header-scroll")) {
+                        headerElement.style.cssText = `transition-duration: 0s;`;
+                        headerElement.classList.add("_header-scroll");
+                        headerItemHeight = headerElement.offsetHeight;
+                        headerElement.classList.remove("_header-scroll");
+                        setTimeout((() => {
+                            headerElement.style.cssText = ``;
+                        }), 0);
+                    } else headerItemHeight = headerElement.offsetHeight;
+                }
+                let options = {
+                    speedAsDuration: true,
+                    speed,
+                    header: headerItem,
+                    offset: offsetTop,
+                    easing: "easeOutQuad"
+                };
+                document.documentElement.classList.contains("menu-open") ? menuClose() : null;
+                if (typeof SmoothScroll !== "undefined") (new SmoothScroll).animateScroll(targetBlockElement, "", options); else {
+                    let targetBlockElementPosition = targetBlockElement.getBoundingClientRect().top + scrollY;
+                    targetBlockElementPosition = headerItemHeight ? targetBlockElementPosition - headerItemHeight : targetBlockElementPosition;
+                    targetBlockElementPosition = offsetTop ? targetBlockElementPosition - offsetTop : targetBlockElementPosition;
+                    window.scrollTo({
+                        top: targetBlockElementPosition,
+                        behavior: "smooth"
+                    });
+                }
+                functions_FLS(`[gotoBlock]: Юхуу...їдемо до ${targetBlock}`);
+            } else functions_FLS(`[gotoBlock]: Йой... Такого блоку немає на сторінці: ${targetBlock}`);
+        };
         let formValidate = {
             getErrors(form) {
                 let error = 0;
@@ -10810,6 +10855,51 @@
             initSliders();
         }));
         let addWindowScrollEvent = false;
+        function pageNavigation() {
+            document.addEventListener("click", pageNavigationAction);
+            document.addEventListener("watcherCallback", pageNavigationAction);
+            function pageNavigationAction(e) {
+                if (e.type === "click") {
+                    const targetElement = e.target;
+                    if (targetElement.closest("[data-goto]")) {
+                        const gotoLink = targetElement.closest("[data-goto]");
+                        const gotoLinkSelector = gotoLink.dataset.goto ? gotoLink.dataset.goto : "";
+                        const noHeader = gotoLink.hasAttribute("data-goto-header") ? true : false;
+                        const gotoSpeed = gotoLink.dataset.gotoSpeed ? gotoLink.dataset.gotoSpeed : 500;
+                        const offsetTop = gotoLink.dataset.gotoTop ? parseInt(gotoLink.dataset.gotoTop) : 0;
+                        if (modules_flsModules.fullpage) {
+                            const fullpageSection = document.querySelector(`${gotoLinkSelector}`).closest("[data-fp-section]");
+                            const fullpageSectionId = fullpageSection ? +fullpageSection.dataset.fpId : null;
+                            if (fullpageSectionId !== null) {
+                                modules_flsModules.fullpage.switchingSection(fullpageSectionId);
+                                document.documentElement.classList.contains("menu-open") ? menuClose() : null;
+                            }
+                        } else gotoblock_gotoBlock(gotoLinkSelector, noHeader, gotoSpeed, offsetTop);
+                        e.preventDefault();
+                    }
+                } else if (e.type === "watcherCallback" && e.detail) {
+                    const entry = e.detail.entry;
+                    const targetElement = entry.target;
+                    if (targetElement.dataset.watch === "navigator") {
+                        document.querySelector(`[data-goto]._navigator-active`);
+                        let navigatorCurrentItem;
+                        if (targetElement.id && document.querySelector(`[data-goto="#${targetElement.id}"]`)) navigatorCurrentItem = document.querySelector(`[data-goto="#${targetElement.id}"]`); else if (targetElement.classList.length) for (let index = 0; index < targetElement.classList.length; index++) {
+                            const element = targetElement.classList[index];
+                            if (document.querySelector(`[data-goto=".${element}"]`)) {
+                                navigatorCurrentItem = document.querySelector(`[data-goto=".${element}"]`);
+                                break;
+                            }
+                        }
+                        if (entry.isIntersecting) navigatorCurrentItem ? navigatorCurrentItem.classList.add("_navigator-active") : null; else navigatorCurrentItem ? navigatorCurrentItem.classList.remove("_navigator-active") : null;
+                    }
+                }
+            }
+            if (getHash()) {
+                let goToHash;
+                if (document.querySelector(`#${getHash()}`)) goToHash = `#${getHash()}`; else if (document.querySelector(`.${getHash()}`)) goToHash = `.${getHash()}`;
+                goToHash ? gotoblock_gotoBlock(goToHash, true, 500, 20) : null;
+            }
+        }
         function headerScroll() {
             addWindowScrollEvent = true;
             const header = document.querySelector("header.header");
@@ -11183,6 +11273,38 @@
                     submenu.style.maxHeight = "auto";
                 }));
             }
+            if (document.querySelector(".nav-page-reviews__item") && document.querySelector(".item-page-reviews")) {
+                const tabButtons = document.querySelectorAll(".nav-page-reviews__item");
+                const tabItems = document.querySelectorAll(".item-page-reviews");
+                function updateAfter(element) {
+                    const left = element.offsetLeft;
+                    const width = element.offsetWidth;
+                    document.documentElement.style.setProperty("--after-left", `${left}px`);
+                    document.documentElement.style.setProperty("--after-width", `${width}px`);
+                }
+                tabButtons.forEach((button => {
+                    button.addEventListener("click", (() => {
+                        const filter = button.getAttribute("data-filter");
+                        tabButtons.forEach((btn => btn.classList.remove("active")));
+                        button.classList.add("active");
+                        updateAfter(button);
+                        tabItems.forEach((item => {
+                            if (filter === "all" || item.getAttribute("data-category") === filter) item.style.display = "grid"; else item.style.display = "none";
+                        }));
+                    }));
+                }));
+                const activeButton = document.querySelector(".nav-page-reviews__item.active");
+                if (activeButton) updateAfter(activeButton); else {
+                    const firstButton = tabButtons[0];
+                    firstButton.classList.add("active");
+                    updateAfter(firstButton);
+                }
+                tabItems.forEach((item => {
+                    setTimeout((() => {
+                        item.classList.add("show");
+                    }), 10);
+                }));
+            }
             document.addEventListener("click", (e => {
                 const targetElement = e.target;
                 if (document.querySelector(".lang-header") && isTouchScreen) if (targetElement.closest(".lang-header")) document.documentElement.classList.toggle("lang-open"); else if (!targetElement.closest(".lang-header")) document.documentElement.classList.remove("lang-open");
@@ -11239,6 +11361,7 @@
         showMore();
         formQuantity();
         formRating();
+        pageNavigation();
         headerScroll();
     })();
 })();
